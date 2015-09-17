e16a56c1415d32e8ad7877a3ca188f07454797cd
JinXin <JinXin@JinXin-PC>
2015-09-17 22:12:53 +0800

    新增POI包

---
+++ JiBanChaXun/src/jiban/ExcelTool.java
@@ -0,0 +1,17 @@
+package jiban;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelTool {
+	
+	private static Map<String, List<String>> maps = null;
+	
+	public static Map<String, List<String>> parseData() {
+		if(null==maps) {
+			
+		}
+		
+		return maps;
+	}
+}
